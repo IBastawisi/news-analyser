022ea245a4d5e63a80116f9248de6452e9bb0b15
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2021-09-10 18:36:45 +0200

    add testing script

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,42 +1,61 @@
 function handleSubmit(event) {
   event.preventDefault()
   const url = event.target.url.value
-  let btn = event.target.querySelector("button[type=submit]")
+  const btn = event.target.querySelector("button[type=submit]")
+
   btn.disabled = true
   btn.textContent = "Processing.."
-  fetch('/api/analyse?url=' + url)
-    .then(res => res.json())
-    .then(function (result) {
-      btn.textContent = "Submit"
-      btn.disabled = false
 
-      const card = document.createElement('div');
-      card.className = 'card';
-
-      if (result.status.code === "0") {
-        card.insertAdjacentHTML('beforeend', '<h3>Concept List</h3>')
-        result.sentimented_concept_list.forEach(({ form }) => card.insertAdjacentHTML('beforeend', `<span>${form}</span>`))
-
-        card.insertAdjacentHTML('beforeend', '<h3>Entity List</h3>')
-        result.sentimented_entity_list.forEach(({ form }) => card.insertAdjacentHTML('beforeend', `<span>${form}</span>`))
-      } else {
-        card.insertAdjacentHTML('beforeend', `<h3>${result.status.msg}</h3>`)
-      }
-
-
-      document.getElementById('results').lastElementChild.replaceWith(card)
-    }).catch(error => {
-      btn.textContent = "Submit"
-      btn.disabled = false
-
-      const card = document.createElement('div');
-      card.className = 'card';
-
-      card.insertAdjacentHTML('beforeend', `<h3>${error.message}</h3>`)
-
-      document.getElementById('results').lastElementChild.replaceWith(card)
-
-    })
+  fetchAnalysis(url, renderResult, renderError);
 }
 
+const fetchAnalysis = async (url, handleSuccess, handleError) => {
+  try {
+    const response = await fetch('/api/analyse?url=' + url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    let result = await response.json();
+    handleSuccess(result);
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+const renderResult = result => {
+  const btn = document.querySelector("form button[type=submit]")
+  btn.textContent = "Submit"
+  btn.disabled = false
+
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  if (result.status.code === "0") {
+    card.insertAdjacentHTML('beforeend', '<h3>Concept List</h3>')
+    result.sentimented_concept_list.forEach(({ form }) => card.insertAdjacentHTML('beforeend', `<span>${form}</span>`))
+
+    card.insertAdjacentHTML('beforeend', '<h3>Entity List</h3>')
+    result.sentimented_entity_list.forEach(({ form }) => card.insertAdjacentHTML('beforeend', `<span>${form}</span>`))
+  } else {
+    card.insertAdjacentHTML('beforeend', `<h3>${result.status.msg}</h3>`)
+  }
+
+
+  document.getElementById('results').replaceChildren(card)
+
+}
+
+const renderError = error => {
+  const btn = document.querySelector("form button[type=submit]")
+  btn.textContent = "Submit"
+  btn.disabled = false
+
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.insertAdjacentHTML('beforeend', `<h3>${error.message}</h3>`)
+
+  document.getElementById('results').replaceChildren(card)
+
+}
 export { handleSubmit }
---
+++ src/client/tests/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../js/formHandler";
+
+describe('Testing formHandler', () => {
+  test('handleSubmit() function is defined', () => {
+    expect(handleSubmit).toBeDefined()
+  })
+})
